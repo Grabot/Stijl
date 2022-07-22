3f2c1a10fe55a3665f325f17bc7031ad826e17ab
SanderKools-Ordina <sander.kools@ordina.nl>
2022-07-22 15:10:58 +0200

    commented the colorbrewer colouring scheme

--- util/colours.py
+++ util/colours.py
@@ -13,17 +13,17 @@ colours = [
 ]
 
 # Qualitative colour scheme selected from https://colorbrewer2.org
-colours = [
-    "#8dd3c7",
-    "#ffffb3",
-    "#bebada",
-    "#fb8072",
-    "#80b1d3",
-    "#fdb462",
-    "#b3de69",
-    "#fccde5",
-    "#d9d9d9",
-    "#bc80bd",
-    "#ccebc5",
-    "#ffed6f",
-]
+# colours = [
+#     "#8dd3c7",
+#     "#ffffb3",
+#     "#bebada",
+#     "#fb8072",
+#     "#80b1d3",
+#     "#fdb462",
+#     "#b3de69",
+#     "#fccde5",
+#     "#d9d9d9",
+#     "#bc80bd",
+#     "#ccebc5",
+#     "#ffed6f",
+# ]
